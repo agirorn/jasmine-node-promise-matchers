54f56ee12822b901c2f810effbd3fd83700eeb3a
Ægir Örn Símonarson <agirorn@gmail.com>
2016-10-02 19:09:21 +0000

    Fix jasmine.any matchers output in failing specs.

--- spec/to-string.spec.js
+++ spec/to-string.spec.js
@@ -1,2 +1,11 @@
+var toString = require('../lib/to-string');
+
 describe('toString(object)', function() {
+  it('returns jasmine.any(Number) as <jasmine.any(Number)>', function() {
+    expect(toString(jasmine.any(Number))).toEqual('<jasmine.any(Number)>');
+  });
+
+  it('returns 10 as string', function() {
+    expect(toString(10)).toEqual('10');
+  });
 });
